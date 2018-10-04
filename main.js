875a1f250254d96400fb1ba783f9a02f950f8764
RattMoth <43834365+RattMoth@users.noreply.github.com>
2018-10-03 20:26:11 -0700

    Added initial files

---
+++ main.js
@@ -0,0 +1,20 @@
+function translatePigLatin(str, out) {
+  str = document.querySelector('#input').value.toLowerCase();
+  const regex = /[aeiou]/; // regex set to vowels
+  if (str[0].match(regex)) {
+    // if word begins with vowel
+    out = `${str}way`;
+  } else {
+    const index = str.indexOf(str.match(regex)); // find index of first vowel
+    const first = str.slice(0, index); // slice string before vowel
+    const last = str.slice(index); // slice string after vowel
+    out = `${last + first}ay`; // concat and return
+  }
+
+  document.querySelector('#result').innerHTML = out;
+}
+
+function multi(str) {
+  const arr = str.split(' ');
+  arr.forEach(element => translatePigLatin());
+}
