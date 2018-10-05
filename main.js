80f347c253bf66a14a50b78947dd19026b967fb0
Matt <mattanator94@gmail.com>
2018-10-05 14:31:29 -0700

    Added functionality for sentence strings. Will now translate each word in the input string.

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-function translatePigLatin(str, out) {
-  str = document.querySelector('#input').value.toLowerCase();
+function translateWord(word) {
   const regex = /[aeiou]/; // regex set to vowels
-  if (str[0].match(regex)) {
+  if (word[0].match(regex)) {
     // if word begins with vowel
-    out = `${str}way`;
-  } else {
-    const index = str.indexOf(str.match(regex)); // find index of first vowel
-    const first = str.slice(0, index); // slice string before vowel
-    const last = str.slice(index); // slice string after vowel
-    out = `${last + first}ay`; // concat and return
+    return `${word}way`;
   }
+  const index = word.indexOf(word.match(regex)); // find index of first vowel
+  const first = word.slice(0, index); // slice string before vowel
+  const last = word.slice(index); // slice string after vowel
+  return `${last + first}ay`; // concat and return
+}
 
+/* todo: sentence functionality works as expected
+Now deal with capital leters
+
+*/
+
+function translatePigLatin(str) {
+  str = document.querySelector('#input').value;
+  const sentenceArr = str.toLowerCase().split(' ');
+  const out = sentenceArr.map(word => translateWord(word)).join(' ');
   document.querySelector('#result').innerHTML = out;
 }
